7e783071f561c3dee4450ae48bf95b0cafb190d3
Maly <migsking@naver.com>
2020-06-08 19:55:11 +0900

    req to nodes

--- fluffy_parakeet/apis.py
+++ fluffy_parakeet/apis.py
@@ -1,11 +1,24 @@
-from fastapi import Request
+import requests
+
+from fastapi import Request, Response
 
 
 def create_api(destination_address: str, method: str):
-    async def api(request: Request):
-        return {
-            "header": request.headers.raw,
-            "body": request.body
-            }
+    async def api(req: Request):
+        headers = {}
+        for i in req._headers._list:
+            headers[i[0].decode()] = i[1].decode()
+
+        headers["host"] = destination_address
+
+        payload = await req.body()
+        request = requests.Request(req.method, destination_address, headers=headers)
+        request = request.prepare()
+        request.body = payload
+        
+        s = requests.session()
+        res = s.send(request)
+
+        return res.text
 
     return api
