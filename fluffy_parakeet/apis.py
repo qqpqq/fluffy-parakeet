580a3e7eecd8bd1dc06063355dd8ae1822d4f4d7
Maly <migsking@naver.com>
2020-06-08 13:25:49 +0900

    custom api

--- fluffy_parakeet/apis.py
+++ fluffy_parakeet/apis.py
@@ -1,2 +1,7 @@
-async def api():
-    return {"hello":"mally"}
+import requests
+
+def create_api(destination_address):
+    async def api():
+        return requests.get(destination_address).text
+
+    return api
--- fluffy_parakeet/app.py
+++ fluffy_parakeet/app.py
@@ -5,7 +5,7 @@ import uvicorn
 import random
 import requests
 
-from fluffy_parakeet.apis import api
+from fluffy_parakeet.apis import create_api
 from fastapi import FastAPI
 from starlette.endpoints import HTTPEndpoint
 
@@ -19,14 +19,16 @@ class FluffyParakeet(object):
         return self.app
 
     def run(self, host="127.0.0.1", port=8000):
-        self.set_route([{"address":"/", "methods":["GET"]},{"address":"/test", "methods":["POST"]}])
+        self.set_route([{"path":"/", "methods":["GET"],"d_address":"http://www.naver.com"},{"path":"/test", "methods":["POST"], "d_address":"http://www.naver.com"}])
         uvicorn.run(self.app, host=host, port=port)
 
     def set_route(self, routes: list):
         for route in routes:
             hash = hex(random.getrandbits(int(str(time.time())[-3:-1])))[4:]
+            api = create_api(route["d_address"])
+
             self.app.add_api_route(
-                path = route["address"],
+                path = route["path"],
                 endpoint = type(hash, (HTTPEndpoint,), {"api": api}).api,
                 methods = route["methods"]
             )
