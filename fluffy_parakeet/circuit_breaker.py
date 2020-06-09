afa9f2d0c6ff50a688c421ac43214b31e2b8b9dd
Maly <migsking@naver.com>
2020-06-09 17:38:41 +0900

    [CRATE] <exception.py> Create an exception for broken circuit

--- fluffy_parakeet/circuit_breaker.py
+++ fluffy_parakeet/circuit_breaker.py
@@ -3,9 +3,12 @@ import asyncio
 
 from typing import Callable
 from fluffy_parakeet.json_parser import json_parser
-from fluffy_parakeet.exception import BreakedCircuit, Timeout
+from fluffy_parakeet.exception import BrokenCircuit
 
-breaked_circuit = []
+# temporary storage
+broken_circuit = []
 
 def circuit_breaker(destination_address):
-    breaked_circuit.append(destination_address)
+    broken_circuit.append(destination_address)
+    # TODO
+    # circuit break system
